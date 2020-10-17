d0de6555318dce8b1c4a4ce73a04f1122bee880e
Siscu Cano <siscuca@gmail.com>
2020-10-17 16:44:43 +0200

    More test

--- staff/siscu-cano/caray/test/caray-every.test.js
+++ staff/siscu-cano/caray/test/caray-every.test.js
@@ -3,73 +3,109 @@ console.group(`${iconTesting} Testing ---> Caray.prototype.every():`);
 // TEST 1 contain the letter a
 //----------------------------------------------------------------------------------------------------
 (function() {
-    console.log(' should return true for pilar, aida, marta and caterina, checking they contain the letter a')
+    var explanation = 'should return true for pilar, aida, marta and caterina, checking they contain the letter a (Test 1)';
+    var reason1 = 'should result be true';
+    var reason2 = 'should iterations count be 4';
+    var conditionTest1 = false;
+    var conditionTest2 = false;
 
-    var c = new Caray
-    c[0] = 'pilar'
-    c[1] = 'aida'
-    c[2] = 'marta'
-    c[3] = 'caterina'
-    c.length = 4
+    var c = new Caray;
+    c[0] = 'pilar';
+    c[1] = 'aida';
+    c[2] = 'marta';
+    c[3] = 'caterina';
+    c.length = 4;
 
-    var iterations = 0
+    var iterations = 0;
 
     var result = c.every(function(name) {
         iterations++
-
         return name.includes('a')
     })
 
-    console.assert(result === true, 'should result be true')
-    console.assert(iterations === 4, 'should iterations count be 4')
+    console.log(`%c - ${explanation}`, 'color: grey');
+
+    // Condition 1
+    conditionTest1 = result === true;
+    conditionTest1 && console.log(`%c \t ${iconOk} ${reason1}`, 'color: green');
+    console.assert(conditionTest1, { result, reason1 });
+
+    // Condition 2
+    conditionTest2 = iterations === 4;
+    conditionTest2 && console.log(`%c \t ${iconOk} ${reason2}`, 'color: green');
+    console.assert(conditionTest2, { iterations, reason2 });
+
 })();
 
 //----------------------------------------------------------------------------------------------------
 // TEST 2 contain the letter i
 //----------------------------------------------------------------------------------------------------
 (function() {
-    console.log(' should return false for pilar, aida, marta and caterina, checking they contain the letter i')
+    var explanation = 'should return false for pilar, aida, marta and caterina, checking they contain the letter i (Test 2)';
+    var reason1 = 'should result be false';
+    var reason2 = 'should iterations count be 3';
+    var conditionTest1 = false;
+    var conditionTest2 = false;
 
-    var c = new Caray
-    c[0] = 'pilar'
-    c[1] = 'aida'
-    c[2] = 'marta'
-    c[3] = 'caterina'
-    c.length = 4
+    var c = new Caray;
+    c[0] = 'pilar';
+    c[1] = 'aida';
+    c[2] = 'marta';
+    c[3] = 'caterina';
+    c.length = 4;
 
-    var iterations = 0
+    var iterations = 0;
 
     var result = c.every(function(name) {
-        iterations++
+        iterations++;
 
-        return name.includes('i')
+        return name.includes('i');
     })
 
-    console.assert(result === false, 'should result be false')
-    console.assert(iterations === 3, 'should iterations count be 3')
+    console.log(`%c - ${explanation}`, 'color: grey');
+
+    // Condition 1
+    conditionTest1 = result === false;
+    conditionTest1 && console.log(`%c \t ${iconOk} ${reason1}`, 'color: green');
+    console.assert(conditionTest1, { result, reason1 });
+
+    // Condition 2
+    conditionTest2 = iterations === 3;
+    conditionTest2 && console.log(`%c \t ${iconOk} ${reason2}`, 'color: green');
+    console.assert(conditionTest2, { iterations, reason2 });
 })();
 
 //----------------------------------------------------------------------------------------------------
 // TEST 3 non-function callback
 //----------------------------------------------------------------------------------------------------
 (function() {
-    console.log(' should fail on non-function callback')
+    var explanation = 'should fail on non-function callback (Test 3)';
+    var reason1 = 'should fail be defined';
+    var reason2 = 'should fail be instance of TypeError';
+    var conditionTest1 = false;
+    var conditionTest2 = false;
 
-    var callback = [1, true, 'string', null, undefined, {}].random()
-
-    var c = new Caray
-
-    var fail
+    var callback = [1, true, 'string', null, undefined, {}].random();
+    var c = new Caray;
+    var _error;
 
     try {
         c.every(callback)
     } catch (error) {
-        fail = error
+        _error = error
     }
 
-    //console.assert(fail != undefined, 'should fail be defined')
-    console.assert(fail, 'should fail be defined')
-    console.assert(fail instanceof TypeError, 'should fail be instance of TypeError')
+    console.log(`%c - ${explanation}`, 'color: grey');
+
+    // Condition 1
+    conditionTest1 = _error;
+    conditionTest1 && console.log(`%c \t ${iconOk} ${reason1}`, 'color: green');
+    console.assert(conditionTest1, { _error, reason1 });
+
+    // Condition 2
+    conditionTest2 = _error instanceof TypeError;
+    conditionTest2 && console.log(`%c \t ${iconOk} ${reason2}`, 'color: green');
+    console.assert(conditionTest2, { _error, reason2 });
 })();
 
 //----------------------------------------------------------------------------------------------------
--- staff/siscu-cano/caray/test/caray-map.test.js
+++ staff/siscu-cano/caray/test/caray-map.test.js
@@ -3,40 +3,62 @@ console.group(`${iconTesting} Testing ---> Caray.prototype.map():`);
 // TEST 1 map numbers multiplied by 10
 //----------------------------------------------------------------------------------------------------
 (function() {
-    console.log(' should map numbers multiplied by 10')
+    var explanation = 'should map numbers multiplied by 10 (Test 1)';
+    var reason1 = 'should result be instance of Caray';
+    var reason2 = 'should result length match the original Caray';
+    var reason3 = 'should iterations be 5';
+    var reason4 = 'should all elements on results equal to c*10';
+    var conditionTest1 = false;
+    var conditionTest2 = false;
+    var conditionTest3 = false;
+    var conditionTest4 = false;
 
-    var c = new Caray
+    var c = new Caray;
 
-    c[0] = Math.random()
-    c[1] = Math.random()
-    c[2] = Math.random()
-    c[3] = Math.random()
-    c[4] = Math.random()
+    c[0] = Math.random();
+    c[1] = Math.random();
+    c[2] = Math.random();
+    c[3] = Math.random();
+    c[4] = Math.random();
 
-    c.length = 5
+    c.length = 5;
 
-    var iterations = 0
+    var iterations = 0;
 
     var result = c.map(function(item) {
-        iterations++
-
-        return item * 10
+        iterations++;
+        return item * 10;
     })
 
-    console.assert(result instanceof Caray, 'should result be instance of Caray')
-    console.assert(result.length === 5, 'should result length match the original Caray')
+    console.log(`%c - ${explanation}`, 'color: grey');
 
-    console.assert(iterations === 5)
+    // Condition 1
+    conditionTest1 = result instanceof Caray;
+    conditionTest1 && console.log(`%c \t ${iconOk} ${reason1}`, 'color: green');
+    console.assert(conditionTest1, { result, reason1 });
 
-    /*
-    console.assert(result[0] === c[0] * 10)
-    console.assert(result[1] === c[1] * 10)
-    console.assert(result[2] === c[2] * 10)
-    console.assert(result[3] === c[3] * 10)
-    console.assert(result[4] === c[4] * 10)
-    */
-    for (var i = 0; i < c.length; i++)
-        console.assert(result[i] === c[i] * 10)
+    // Condition 2
+    conditionTest2 = result.length === 5;
+    conditionTest2 && console.log(`%c \t ${iconOk} ${reason2}`, 'color: green');
+    console.assert(conditionTest2, { result, reason2 });
+
+    // Condition 3
+    conditionTest3 = iterations === 5;
+    conditionTest3 && console.log(`%c \t ${iconOk} ${reason3}`, 'color: green');
+    console.assert(conditionTest3, { result, reason3 });
+
+    // Condition 4
+    for (var i = 0; i < c.length; i++) {
+        if (result[i] !== c[i] * 10) {
+            conditionTest4 = false;
+            break;
+        }
+        conditionTest4 = true;
+    }
+
+
+    conditionTest4 && console.log(`%c \t ${iconOk} ${reason4}`, 'color: green');
+    console.assert(conditionTest4, { result, c, reason4 });
 })();
 
 //----------------------------------------------------------------------------------------------------
